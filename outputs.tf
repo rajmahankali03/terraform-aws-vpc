7bdbe36e0564b1cda8f141e5c5cf5252e6f62637
RajMahankali <rajmahankali@gmail.com>
2025-08-17 20:31:09 +0530

    sd

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "vpc_id" {
+/*output "vpc_id" {
   value       = aws_vpc.main.id
 }
 output "public_subnet_ids" {
@@ -11,4 +11,4 @@ output "private_subnet_ids" {
 
 output "database_subnet_ids" {
   value = aws_subnet.database[*].id
-}
+}*/
